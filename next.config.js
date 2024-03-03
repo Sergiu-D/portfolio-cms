74a82cd70f9428f5f7c6f28c3901deb8864024de
Sergiu-D <61351750+Sergiu-D@users.noreply.github.com>
2024-03-03 10:52:15 +0200

    update routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const ContentSecurityPolicy = require('./csp')
-// const redirects = require('./redirects')
+const ContentSecurityPolicy = require('./csp')
+const redirects = require('./redirects')
 const path = require('path')
 const { withPayload } = require('@payloadcms/next-payload')
 
@@ -50,7 +50,48 @@ const { withPayload } = require('@payloadcms/next-payload')
 // }
 const nextConfig = withPayload(
   {
-    // your Next config here
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
+        .filter(Boolean)
+        .map(url => url.replace(/https?:\/\//, '')),
+    },
+    redirects,
+    async headers() {
+      const headers = []
+
+      // Prevent search engines from indexing the site if it is not live
+      // This is useful for staging environments before they are ready to go live
+      // To allow robots to crawl the site, use the `NEXT_PUBLIC_IS_LIVE` env variable
+      // You may want to also use this variable to conditionally render any tracking scripts
+      if (!process.env.NEXT_PUBLIC_IS_LIVE) {
+        headers.push({
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+          source: '/:path*',
+        })
+      }
+
+      // Set the `Content-Security-Policy` header as a security measure to prevent XSS attacks
+      // It works by explicitly whitelisting trusted sources of content for your website
+      // This will block all inline scripts and styles except for those that are allowed
+      headers.push({
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy,
+          },
+        ],
+      })
+
+      return headers
+    },
   },
   {
     // The second argument to `withPayload`
@@ -58,13 +99,13 @@ const nextConfig = withPayload(
     // and configure the admin route to your Payload CMS.
 
     // Point to your Payload config (required)
-    configPath: path.resolve(__dirname, './payload/payload.config.ts'),
+    configPath: path.resolve(__dirname, '.src/payload/payload.config.ts'),
 
     // Point to custom Payload CSS (optional)
-    cssPath: path.resolve(__dirname, './css/my-custom-payload-styles.css'),
+    cssPath: path.resolve(__dirname, '.src/css/my-custom-payload-styles.css'),
 
     // Point to your exported, initialized Payload instance (optional, default shown below`)
-    payloadPath: path.resolve(process.cwd(), './payload/payloadClient.ts'),
+    payloadPath: path.resolve(process.cwd(), '.src/payload/payloadClient.ts'),
 
     // Set a custom Payload admin route (optional, default is `/admin`)
     // NOTE: Read the "Set a custom admin route" section in the payload/next-payload README.
